23ec8712477bec53dfffc4d6ffa7d83095d856e7
Belen Medina Roldan <bmedinaroldan@gmail.com>
2021-01-24 20:48:52 -0300

    Se modifica vista index y su controller

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,8 +2,20 @@ const productsDB = require('../data/productsDB');
 
 module.exports = {
     index: (req, res) => {
-        res.render('index', {
+        const toThousand = n => n. toString() . replace( /\B(?=(\d{3})+(?!\d))/g, "." ) ;
+        const calculateDiscount = (discount, price) => {
+            return price - (discount * price / 100);
+        }
 
-        })
+        let inSale = productsDB.filter(product => product.category == "in-sale");
+        let visited = productsDB.filter(product => product.category == "visited");
+
+        res.render('index', {
+            title: "Mercado Liebre Argentina",
+            productsInSale: inSale,
+            visitedProducts: visited,
+            calculateDiscount,
+            toThousand
+        });
     }
 }
